678bc501b1b56903d567912c6ad22491937b1feb
StarHeartY <yirui.pre@gmail.com>
2026-06-27 22:15:49 +0800

    feat: init docs site

---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <strong>CalculatorX 帮助中心</strong>,
+  project: {
+    link: 'https://github.com/你的github用户名/calcx-docs' // 右上角的 GitHub 图标链接
+  },
+  docsRepositoryBase: 'https://github.com/你的github用户名/calcx-docs/tree/main', // 用于“编辑此页”功能
+  footer: {
+    text: '© 2026 CalculatorX Project'
+  },
+  // 强制全站默认主题色跟随系统，不闪烁
+  nextThemes: {
+    defaultTheme: 'system'
+  }
+}
